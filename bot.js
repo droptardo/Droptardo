52a477d8a9369d31932df9de5cadd85acc827164
droptardo <38538910+droptardo@users.noreply.github.com>
2018-04-19 15:26:26 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,37 +11,6 @@ client.on('message', message => {
   	}
 });
 
-var locations = [
-  "Tilted Towers",
-  "Snobby Shores",
-  "Shifty Shafts",
-  "Pleasant Park",
-  "Retail Row",
-  "Junk Junction",
-  "Haunted Hills",
-  "Flush Factory",
-  "Fatal Fields",
-  "Moisty Mire",
-  "Lonely Lodge",
-  "Wailing Woods",
-  "M'Laika",
-  "Anarchy Acres",
-  "Tomato Town",
-  "Salty Springs",
-  "Greasy Grove"
-];
-
-var lock = function() {
-    var loc = locations[Math.floor(Math.random() * locations.length)]
-    return var(lock)
-}
-
-client.on('message', message => {
-    if (message.content === '!drop') {
-    	message.reply(lock());
-  	}
-});
-
 // storing jokes in a variable jokes
 
 var jokes = [
